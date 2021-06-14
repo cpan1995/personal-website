84be88b28142105df74d0f30a0a3208604dddc06
cpan1995 <cpan1995@gmail.com>
2021-06-14 19:01:45 -0400

    fixed the arrow buttons and css for pages

--- script.js
+++ script.js
@@ -18,39 +18,54 @@ function pageInit(pages){
 }
 
 //Cycling through pages with the arrow key
-function pageCycle(e, pages){
-    e.toElement.style.borderRightColor = 'white';
-    e.toElement.style.borderBottomColor = 'white';
-    //updateArrowUpdater();
+function pageCycleRight(e, pages){
     pages[pageNodeCounter].style.display = 'none';
-    console.log(pages);
-    console.log(pageNodeCounter);
     pages[pageNodeCounter + 1].style.display = 'inline';
     pageNodeCounter++;
+    updateArrowUpdater();
 }
 
+function pageCycleLeft(e, pages){
+    pages[pageNodeCounter].style.display = 'none';
+    pages[pageNodeCounter - 1].style.display = 'inline';
+    pageNodeCounter--;
+    updateArrowUpdater();
+}
+
+function arrowMouseOver(e){
+    e.toElement.style.borderRightColor = 'white';
+    e.toElement.style.borderBottomColor = 'white';
+}
+function arrowMouseOut(e){
+    e.toElement.style.borderRightColor = 'black';
+    e.toElement.style.borderBottomColor = 'black';
+}
 //Updates the arrow as well as the current page counter
 function updateArrowUpdater(){
 
-    switch(pageNodeCounter) {
-        case (pageNodeCounter > pages.length - 1):
-            pageNodeCounter = 0;
-            break;
-        case (pageNodeCounter == 0):
-            leftArrow.style.display = 'none';
-            rightArrow.style.display = 'inline'
-            break;
-        case (pageNodeCounter != 0):
-            leftArrow.style.display = 'inline';
-            break;
-        case (pageNodeCounter = pages.length - 1):
+    if(pageNodeCounter >= pages.length - 1){
+        console.log(pageNodeCounter);
             rightArrow.style.display = 'none';
             leftArrow.style.display = 'inline';
-            break;
+    }
+    else if(pageNodeCounter == 0){
+        leftArrow.style.display = 'none';
+        rightArrow.style.display = 'inline'
+    }
+    else if(pageNodeCounter != 0){
+        leftArrow.style.display = 'inline';
+    }
+    else{
+        leftArrow.style.display = 'inline';
+        rightArrow.style.display  = 'inline';
     }
 }
 rightArrow.addEventListener('click', function(event){
-    pageCycle(event, pages);
+    pageCycleRight(event, pages);
 });
+
+leftArrow.addEventListener('click', function(event){
+    pageCycleLeft(event, pages);
+})
 
 pageInit(pages);
