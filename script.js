07cadeb513d03d58c01a3d47c661b7402ebf58eb
cpan1995 <cpan1995@gmail.com>
2021-06-14 16:02:04 -0400

    added checkers for the arrow updater

--- script.js
+++ script.js
@@ -1,19 +1,56 @@
 const rightArrow = document.querySelector('.right');
 const leftArrow = document.querySelector('.left');
-
+const pages = document.querySelectorAll('.page');
+let pageNodeCounter = 0;
 console.log(rightArrow);
 
-rightArrow.addEventListener('click', function(event){
-    transformArrow(event);
-});
+//Initialize the first page. Hides all the rest
+function pageInit(pages){
+    for(i = 0; i < pages.length; i++){
+        if(i != 0){
+            pages[i].style.display = 'none';
+        }
+    }
+    if(pageNodeCounter == 0){
+        leftArrow.style.display = 'none';
+    }
+    console.log(pages);
+}
 
-function transformArrow(e){
+//Cycling through pages with the arrow key
+function pageCycle(e, pages){
     e.toElement.style.borderRightColor = 'white';
     e.toElement.style.borderBottomColor = 'white';
+    //updateArrowUpdater();
+    pages[pageNodeCounter].style.display = 'none';
+    console.log(pages);
+    console.log(pageNodeCounter);
+    pages[pageNodeCounter + 1].style.display = 'inline';
+    pageNodeCounter++;
 }
 
-const pages = document.querySelectorAll('.page');
-console.log(pages);
-function pagesOrganizer(pages){
-    
+//Updates the arrow as well as the current page counter
+function updateArrowUpdater(){
+
+    switch(pageNodeCounter) {
+        case (pageNodeCounter > pages.length - 1):
+            pageNodeCounter = 0;
+            break;
+        case (pageNodeCounter == 0):
+            leftArrow.style.display = 'none';
+            rightArrow.style.display = 'inline'
+            break;
+        case (pageNodeCounter != 0):
+            leftArrow.style.display = 'inline';
+            break;
+        case (pageNodeCounter = pages.length - 1):
+            rightArrow.style.display = 'none';
+            leftArrow.style.display = 'inline';
+            break;
+    }
 }
+rightArrow.addEventListener('click', function(event){
+    pageCycle(event, pages);
+});
+
+pageInit(pages);
